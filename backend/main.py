59552dcceaf2ea3226167e4975eba6eeee2a66f4
Joe Randazzo <jprandazzo@icloud.com>
2025-06-26 01:28:28 -0700

    fix typo

--- backend/main.py
+++ backend/main.py
@@ -24,7 +24,7 @@ if settings.environment == "development":
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allowed_origins
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
